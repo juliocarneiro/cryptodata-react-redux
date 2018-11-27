6123c9235bab1a0c68fa55d3d7edecf1e0648c5d
juliocarneiro <juliocarneiro@gmail.com>
2018-11-27 20:37:44 -0200

    first

---
+++ build/precache-manifest.17fd1fa01e2e44b409103ffe525e3ab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "da48d4eca28d332fec92",
+    "url": "/static/js/main.da48d4ec.chunk.js"
+  },
+  {
+    "revision": "a76361f48a99fd120c9a",
+    "url": "/static/js/1.a76361f4.chunk.js"
+  },
+  {
+    "revision": "da48d4eca28d332fec92",
+    "url": "/static/css/main.e32dde13.chunk.css"
+  },
+  {
+    "revision": "98adf8928cc790c3e3c1376f793dae14",
+    "url": "/index.html"
+  }
+];
